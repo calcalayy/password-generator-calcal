7c029132a861baf489ab70e196c94d619a330937
calcalayy <minelandsmc14@gmail.com>
2022-03-16 21:21:29 +0000

    finish
    purr

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-letters = ["a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "-", "+", "=", "_", "f", "F", "g", "G", "h", "H", "[", "]", "I", "i", "j", "k", "K"]
+letters = ["a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "-", "+", "=", "_", "f", "F", "g", "G", "h", "H", "[", "]", "I", "i", "j", "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z", "%", "*", "^"]
 
 # use * operator when printing to do all at once
 
