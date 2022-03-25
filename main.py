3c2b317b7821e689ad2e8723979b20b07712457c
calcalayy <minelandsmc14@gmail.com>
2022-03-25 20:17:44 +0000

    Update main.py
    misc changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-letters = ["a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "-", "+", "=", "_", "f", "F", "g", "G", "h", "H", "[", "]", "I", "i", "j", "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z", "%", "*", "^"]
+letters = ["a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "-", "+", "=", "_", "f", "F", "g", "G", "h", "H", "[", "]", "I", "i", "j", "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z", "%", "*", "^", "!", "~"]
 
 # use * operator when printing to do all at once
 
@@ -10,6 +10,8 @@ letters = ["a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "-", "+", "=", "_",
 numberletters = input("How many letters do you want in your password? Enter here >:) :")
 #converts into int
 numberletters = int(numberletters)
+
+time.sleep(1)
 
 print("Here is your password:")
 
